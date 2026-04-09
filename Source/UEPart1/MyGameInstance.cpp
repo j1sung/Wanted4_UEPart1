5f3e2bbd9bbb0c088f2495465d7a83004009f84a
dalssak <dalssagi00@gmail.com>
2026-04-09 16:40:30 +0900

    언리얼 오브젝트 리플렉션1/2 수업 진행
    - Person/Student/Teacher 클래스 추가
    - UGameInstance::Init() 함수에서 리플렉션 관련 내용 코드 진행

--- Source/UEPart1/MyGameInstance.cpp
+++ Source/UEPart1/MyGameInstance.cpp
@@ -2,28 +2,117 @@
 
 
 #include "MyGameInstance.h"
+#include "Student.h"
+#include "Teacher.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+	// 기본 값은 CDO라는 특별한 템플릿 객체에 저장됨.
+	SchoolName = TEXT("기본학교");
+}
 
 void UMyGameInstance::Init()
 {
 	Super::Init();
+	
+	// 클래스 정보 가져오기.
+	UClass* ClassRuntime = GetClass();
+	UClass* ClassCompile = UMyGameInstance::StaticClass();
 
+	// 어서트.
+	// check(ClassRuntime != ClassCompile); // 크래시까지 발생시키는 어써트.
+	// ensure(ClassRuntime == ClassCompile); // 출력 로그 창에 오류 표시. 한번만.
+	// ensureAlways() // 실행에서 반복적
+
+	// 클래스 이름 출력.
+	UE_LOG(
+		LogTemp, 
+		Log, 
+		TEXT("학교를 담당하는 클래스: %s, %s"),
+		*ClassRuntime->GetName(),
+		*ClassCompile->GetName()
+	);
+
+	SchoolName = TEXT("청강문화산업대학교");
+	UE_LOG(
+		LogTemp,
+		Log,
+		TEXT("학교 이름: %s"),
+		*SchoolName
+	);
+	UE_LOG(
+		LogTemp,
+		Log,
+		TEXT("학교 이름: %s"),
+		*GetClass()->GetDefaultObject<UMyGameInstance>()->SchoolName
+	);
+	// 둘의 값이 다름 -> CDO와 런타임 변수 값이 다름
+
+	UE_LOG(LogTemp, Log, TEXT("====================="));
+
+	// 학생/선생님 객체 생성.
+	UStudent* Student = NewObject<UStudent>();
+	UTeacher* Teacher = NewObject<UTeacher>();
+
+	// 학생 클래스의 Getter/Setter 사용.
+	Student->SetName(TEXT("학생1"));
+	UE_LOG(LogTemp, Log, TEXT("새로운 학생 이름: %s"), *Student->GetName());
+
+	// 언리얼의 리플렉션을 활용해 프로퍼티 정보 설정 및 가져오기.
+	
+	//UTeacher::StaticClass()->FindPropertyByName(TEXT("Name"));
+	FProperty* NameProp = 
+		Teacher->GetClass()->FindPropertyByName(TEXT("Name"));
+	if (NameProp)
+	{
+		// 리플렉션을 활용해 현재 프로퍼티에 저장된 값 가져오기.
+		FString CurrentTeacherName;
+		NameProp->GetValue_InContainer(Teacher, &CurrentTeacherName);
+		UE_LOG(
+			LogTemp,
+			Log,
+			TEXT("현재 선생님 이름: %s"),
+			*CurrentTeacherName
+		);
+
+		// 리플렉션을 활용해 프로퍼티에 새로운 값 저장.
+		FString NewTeacherName = (TEXT("지성"));
+		NameProp->SetValue_InContainer(Teacher, &NewTeacherName);
+		UE_LOG(
+			LogTemp,
+			Log,
+			TEXT("현재 선생님 이름: %s"),
+			*Teacher->GetName()
+		);
+	}
+
+	// 함수 호출 (리플렉션 활용).
+	UFunction* DoLessonFunc 
+		= Teacher->GetClass()->FindFunctionByName(TEXT("DoLesson"));
+
+	if (DoLessonFunc)
+	{
+		Teacher->ProcessEvent(DoLessonFunc, nullptr);
+	}
+	/*
 	// 로그 출력.
 	UE_LOG(LogTemp, Log, TEXT("%s"), TEXT("Hello Unreal"));
 	// L"%s"랑 같은 widechar로 변환 - 16비트 2
 	// TEXT매크로가 ##으로 이어 붙여주는 
 
 	// TCHAR | FString.
-	TCHAR LogCharArray[] = TEXT("Hello Unreal");
+	// TCHAR = wchar_t
+	TCHAR LogCharArray[] = TEXT("Hello Unreal");  // L 붙여주는거랑 똑같다.
 	// UE_LOG 함수.
-	// Log 카테고리(타입).
-	// 로그 수준(Log, Warning, Error).
-	// 포맷 (format, 서식) - 출력할 값의 타입.
+	// 1. Log 카테고리(타입).
+	// 2. 로그 수준(Log, Warning, Error).
+	// 3. 포맷 (format, 서식) - 출력할 값의 타입.
 	// 가변 인자: 포맷에 지정한 타입에 알맞은 값을 전달.
 	UE_LOG(LogTemp, Log, TEXT("%s"), LogCharArray);
 
 	FString LogCharString = LogCharArray;
 	//UE_LOG(LogTemp, Log, TEXT("%s"), LogCharString);
-	// FString은 앞에 *연산자를 붙여야함
+	// FString은 앞에 *연산자를 붙여야함 -> 연산자 오버로딩(operator) 사용됨
 	UE_LOG(LogTemp, Log, TEXT("%s"), *LogCharString);
 
 	// FString에서 TCHAR 포인터를 가져오는 방법.
@@ -97,5 +186,5 @@ void UMyGameInstance::Init()
 		const static FName SearchInNamePool = FName(TEXT("pelvis"));
 		// 읽기 전용이면 const를 잘 쓰자
 
-	}
+	}*/
 }
--- Source/UEPart1/MyGameInstance.h
+++ Source/UEPart1/MyGameInstance.h
@@ -6,14 +6,19 @@
 #include "Engine/GameInstance.h"
 #include "MyGameInstance.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class UEPART1_API UMyGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
 public:
+	UMyGameInstance();
+public:
+	// 게임 인스턴스를 초기화 할때 사용하는 함수.
 	virtual void Init() override;
+
+private:
+	// 학교 이름 변수
+	UPROPERTY()
+	FString SchoolName;
 };
