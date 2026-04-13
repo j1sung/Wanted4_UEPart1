02f380b5e1f7ca78ffe01a9e9a94067432683927
dalssak <dalssagi00@gmail.com>
2026-04-13 15:04:28 +0900

    10강 TArray 활용 실습 진행

--- Source/UEPart1/MyGameInstance.cpp
+++ Source/UEPart1/MyGameInstance.cpp
@@ -7,6 +7,7 @@
 #include "Staff.h"
 #include "Card.h"
 #include "CourseInfo.h" // 전방선언 했으니
+#include "Algo/Accumulate.h"
 
 UMyGameInstance::UMyGameInstance()
 {
@@ -43,6 +44,62 @@ void UMyGameInstance::Init()
 
 	UE_LOG(LogTemp, Log, TEXT("========================"));
 
+	// TArray  사용.
+	const int32 ArrayNum = 10;
+	TArray<int32> Int32Array;
+
+	for (int32 ix = 1; ix <= ArrayNum; ++ix)
+	{
+		Int32Array.Add(ix);
+	}
+
+	// 짝수 제거.
+	Int32Array.RemoveAll(
+		// [] - 캡처 (외부 내용을 람다 안에서 사용할 때 활용).
+		// () - 파라미터.
+		// -> - 반환형.
+		// { } - 본문
+		[](int32 Val) -> bool
+		{
+			return Val % 2 == 0;
+		}
+	);
+
+	// 짝수 삽입.
+	Int32Array += {2, 4, 6, 8, 10};
+
+	// 비교 ( 동등 비교 ).
+	TArray<int32> Int32ArrayCompare;
+	int32 CArray[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
+	Int32ArrayCompare.AddUninitialized(ArrayNum);
+	
+	// C 스타일 배열을 TArray에 메모리 복사.
+	FMemory::Memcpy(
+		Int32ArrayCompare.GetData(),
+		CArray,
+		sizeof(int32) * ArrayNum
+	);
+
+	// 어서트 (크래시를 발생시키지 않고, 출력 로그 창에 오류 메시지 출력).
+	ensureAlways(Int32Array == Int32ArrayCompare);
+
+	// 합계.
+	int32 Sum = 0;
+	for (const int32& Int32Num : Int32Array)
+	{
+		Sum += Int32Num;
+	}
+
+	// 알고리즘 활용 (합계 구하기).
+	int32 SumByAlgo = Algo::Accumulate(Int32Array, 0);
+	ensureAlways(Sum == SumByAlgo);
+
+	UE_LOG(
+		LogTemp,
+		Log,
+		TEXT("Sum = %d | SumByAlgo: %d | Sum == SumByAlgo: %s"),
+		Sum, SumByAlgo, (Sum == SumByAlgo ? TEXT("True") : TEXT("False"))
+	);
 
 	/*
 	// TArray는 언리얼 엔진이 지원하는 동적 배열
