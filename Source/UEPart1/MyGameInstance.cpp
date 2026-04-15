bdb01ff2d72ac6f235850090fcb46868947069ef
dalssak <dalssagi00@gmail.com>
2026-04-15 16:33:08 +0900

    JSON 직렬화 실습 진행
    - Build.cs에 Json 관련 모듈 추가
    - JSON 직렬화/역직렬화 예제 진행

--- Source/UEPart1/MyGameInstance.cpp
+++ Source/UEPart1/MyGameInstance.cpp
@@ -4,6 +4,7 @@
 #include "MyGameInstance.h"
 #include "StudentData.h"
 #include "Student.h"
+#include "JsonObjectConverter.h"
 
 
 UMyGameInstance::UMyGameInstance()
@@ -170,6 +171,85 @@ void UMyGameInstance::Init()
 				*StudentDest->GetName(),
 				StudentDest->GetOrder()
 			);
+		}
+	}
+
+	// JSON(JavaScript Object Notation) 직렬화.
+	{
+		const FString JsonDataFileName(
+			TEXT("StudentJsonData.txt")
+		);
+
+		// 저장할 파일 경로 값.
+		FString JsonDataAbsolutePath
+			= FPaths::Combine(SavePath, JsonDataFileName);
+
+		// 경로 값 정리.
+		FPaths::MakeStandardFilename(JsonDataAbsolutePath);
+
+		// JSON 직렬화 과정.
+		// UObject-> JSON Object -> JSON 문자열 -> 기록
+		TSharedRef<FJsonObject> JsonObjectRef
+			= MakeShared<FJsonObject>();
+
+		// JSON 오브젝트로 변환
+		FJsonObjectConverter::UStructToJsonObject( // 리플랙션 전달/비교
+			StudentSource->StaticClass(),
+			StudentSource,
+			JsonObjectRef
+		);
+
+		// 직렬화.
+		FString JsonOutString;
+		TSharedRef<TJsonWriter<TCHAR>> JsonWriterAr =
+			TJsonWriterFactory<TCHAR>::Create(&JsonOutString);
+
+		if(FJsonSerializer::Serialize(JsonObjectRef, JsonWriterAr))
+		{
+			// 성공한 경우에 파일에 저장
+			FFileHelper::SaveStringToFile(
+				JsonOutString,
+				*JsonDataAbsolutePath
+			);
+		}
+
+		// 역직렬화.
+		FString JsonInString;
+		// 파일에서 문자열로 읽어오기.
+		FFileHelper::LoadFileToString(JsonInString, *JsonDataAbsolutePath);
+
+		UE_LOG(
+			LogTemp,
+			Log,
+			TEXT("[Json] %s"),
+			*JsonInString
+		);
+
+		// 역직렬화를 위한 아카이브 생성.
+		TSharedRef<TJsonReader<TCHAR>> JsonReaderAr
+			= TJsonReaderFactory<TCHAR>::Create(JsonInString);
+
+		TSharedPtr<FJsonObject> JsonObjectDest;
+		if (FJsonSerializer::Deserialize(JsonReaderAr, JsonObjectDest))
+		{
+			// 언리얼 오브젝트 생성 후 변환.
+			UStudent* JsonStudentDest = NewObject<UStudent>();
+			// JsonObjectConverter를 활용해 변환.
+			if (FJsonObjectConverter::JsonObjectToUStruct(
+				JsonObjectDest.ToSharedRef(),
+				UStudent::StaticClass(),
+				JsonStudentDest))
+			{
+				// 출력.
+				UE_LOG(
+					LogTemp,
+					Log,
+					TEXT("[JsonData] 이름: %s, 순번: %d"),
+					*JsonStudentDest->GetName(),
+					JsonStudentDest->GetOrder()
+				);
+
+			}
 		}
 	}
 }
